66146762b77d549453721834653dd0a81ddcc09a
Emilio López <turl@tuxfamily.org>
2011-10-14 22:07:01 -0300

    Code cleanup

--- kka.php
+++ kka.php
@@ -10,74 +10,75 @@ Examples:
 "password1,password2,password3" -- 1 word(there's no white space separator), 1 try to succeed.
 Please note that passphrase IS case sensitive! (PASSWORD is not equal to password)
 */
+
 //Configuration
-$denied_path	 = "/access/"; //Path to protect.
-$passphrase	 = "abra kadabra";  //Your passphrase.
-$close_session	 = "close it"; //the word to close your session.
-$filename_hash	 = sha1( $passphrase.sha1( $_SERVER['REMOTE_ADDR'] ) )."-kka";
-$passphrase 	 = ltrim( $passphrase ); //Trim space at the begining of the string
-$passphrase	 = rtrim( $passphrase ); //Trim space at the end of the string
-$passphrase 	 = explode(" ", $passphrase ); //We create an array of passwords
-$possible_tries	 = count( $passphrase ); //Getting the max quantity of tries.
-$log		 = 0;//Do we log attempts? 1 or 0.
-$log_file	 = "log-" . date('d-m-Y-') . $filename_hash . ".log"; //change it with the name you want
-if($log == 1){
+$passphrase		= 'abra kadabra';				//Your passphrase.
+$close_session	= 'close it';					//the word to close your session.
+$filename_hash	= sha1($passphrase.sha1($_SERVER['REMOTE_ADDR'])).'-kka';
+$passphrase		= trim($passphrase);			//Trim starting/ending whitespace
+$passphrase		= explode(' ', $passphrase);	//We create an array of passwords
+$possible_tries	= count($passphrase );			//Getting the max quantity of tries.
+$log			= false;						//Do we log attempts? 1 or 0.
+$log_file		= 'log-'.date('d-m-Y-').$filename_hash.'.log';
+
+if($log) {
 	//let's log!, Request, IP, timestamp and user-agent by now...
-	$HostInfo_Request 	= "Request:"	. $_SERVER['REQUEST_URI'] 			. "\n";
-	$HostInfo_Proxy 	= "Proxy?:"		. $_SERVER['HTTP_X_FORWARDED_FOR'] 	. "\n";
-	$HostInfo_IP 		= "IP:" 		. $_SERVER['REMOTE_ADDR'] 			. "\n";
-	$HostInfo_TimeStamp = "Timestamp:" 	. date('d-m-Y G:i:s') 				. "\n";
-	$HostInfo_UserAgent = "User-Agent:"	. $_SERVER['HTTP_USER_AGENT'] 		. "\n\n";
-	file_put_contents(
-		$log_file, 
-		$HostInfo_Request . 
-		$HostInfo_Proxy . 
-		$HostInfo_IP . 
-		$HostInfo_TimeStamp . 
-		$HostInfo_UserAgent, FILE_APPEND);
+	$HostInfo	 = 'Request:'	. $_SERVER['REQUEST_URI']			. "\n";
+	$HostInfo	.= 'Proxy?:'		. $_SERVER['HTTP_X_FORWARDED_FOR']	. "\n";
+	$HostInfo	.= 'IP:'			. $_SERVER['REMOTE_ADDR']			. "\n";
+	$HostInfo	.= 'Timestamp:'	. date('d-m-Y G:i:s')				. "\n";
+	$HostInfo	.= 'User-Agent:'	. $_SERVER['HTTP_USER_AGENT']		. "\n\n";
+	file_put_contents($log_file, $HostInfo, FILE_APPEND);
 }
+
 //We get the knock (i.e. /path/?pass we get the "pass" string.)
-$knock = explode("?", $_SERVER['REQUEST_URI'], 2);
-/* 
+if(isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING'])) {
+	$knock = $_SERVER['QUERY_STRING'];
+} elseif(isset($_SERVER['REQUEST_URI']) && !empty($_SERVER['REQUEST_URI']) && strpos('?',$_SERVER['REQUEST_URI']) !== false) {
+	$knock = explode('?', $_SERVER['REQUEST_URI'], 2);
+	$knock = end($knock);
+} else {
+	$knock = '';
+}
+
+/*
 Checking session, if it doesn't exists then we check that /path/
 hasn't any passphrase or "?" without vars and return 404.
 */
-if( file_exists( $filename_hash ) ) {
-	if( file_get_contents( $filename_hash ) != $possible_tries) {
-		if( $_SERVER['REQUEST_URI'] == $denied_path ) {
+if(file_exists( $filename_hash)) {
+	if(file_get_contents($filename_hash) != $possible_tries) {
+		if(empty($knock)) {
 		header('HTTP/1.1 404 Not Found');
 		exit();
 		}
-		elseif ( !$knock[1] ) {
-		header('HTTP/1.1 404 Not Found');
-		exit();
 	}
 }
-}
+
 //End checking
 //If it's a new file, initialize it.
-if( !file_exists($filename_hash) ) {
-	file_put_contents( $filename_hash, "0");
+if(!file_exists($filename_hash)) {
+	file_put_contents($filename_hash, '0');
 }
-$succeed_try = file_get_contents( $filename_hash ); //Counter to match with $possible_tries
+
+$succeed_try = file_get_contents($filename_hash); //Counter to match with $possible_tries
+
 //Let's Knock N' Roll! :)
-if( $knock[1] == $close_session ) {
-		unlink( $filename_hash );
+if($knock == $close_session) {
+		unlink($filename_hash);
 		header('HTTP/1.1 404 Not Found');
 		exit();
-	}
-if( $succeed_try != $possible_tries ) {
-	if( $knock[1] == $passphrase[$succeed_try] ) {
+}
+
+if($succeed_try != $possible_tries) {
+	if($knock == $passphrase[$succeed_try]) {
 		$succeed_try = $succeed_try + 1;
-		file_put_contents( $filename_hash, $succeed_try );
+		file_put_contents($filename_hash, $succeed_try);
 		header('HTTP/1.1 404 Not Found');
 		exit();
-	}
-	else{
+	} else {
 		$succeed_try = 0;
-		file_put_contents( $filename_hash, $succeed_try );
+		file_put_contents($filename_hash, $succeed_try);
 		header('HTTP/1.1 404 Not Found');
 		exit();
 	}
 }
-?>
