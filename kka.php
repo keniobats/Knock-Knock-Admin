8d1a710d0de6ea88616ed5b547b8d48a39e08978
seth <xd.seth@gmail.com>
2011-10-27 00:38:46 +0000

    Code reorganization

--- kka.php
+++ kka.php
@@ -12,13 +12,13 @@
 //	"this example contains numbers 123456" -- 5 words, 5 tries to succeed.
 //	"password1,password2,password3" -- 1 word(there's no white space separator), 1 try to succeed.
 //	Please note that passphrase IS case sensitive! (PASSWORD is not equal to password)
-define('KKA_PASSPHRASE',		'abra kadabra');
+define('KKA_PASSPHRASE', 'abra kadabra');
 
 // The word to close your session. It's goes in only one request
-define('KKA_CLOSE_SESSION',		'close it');
+define('KKA_CLOSE_SESSION', 'close it');
 
 // The file used to save sessions. Don't touch this without being sure of what are you doing!
-define('KKA_FILE_NAME', 		hash('sha256', KKA_PASSPHRASE . hash('sha256', $_SERVER['REMOTE_ADDR'])).'-kka');
+define('KKA_FILE_NAME', hash('sha256', KKA_PASSPHRASE . hash('sha256', $_SERVER['REMOTE_ADDR'])).'-kka');
 
 //  Configure the loging options.
 //	The level can be:
@@ -32,6 +32,53 @@ define('KKA_LOG_EMAIL_LEVEL',	'NONE');
 define('KKA_LOG_EMAIL_ADDRESS',	'example@example.com'); 
 // Stop editing here!!
 
+
+/**
+ * Write log to a file
+ *
+ * @param string $what is the log text
+ */
+function log_to_file($what){
+	file_put_contents(KKA_LOG_FILE_NAME, $what, FILE_APPEND);
+}
+
+/**
+ * Write log to a file
+ *
+ * @param string $what is the log text
+ */
+function log_to_email($what){
+	mail(KKA_LOG_EMAIL_ADDRESS, 'Knock Knock Admin log', $what);
+}
+
+/**
+ * Return timestamp, request, IP and user agent for loging
+ *
+ * @param string $level can be 'ALL' or 'SUCCESS'. It indicates where is the function being called from. Read the configuration documentation.
+ */
+function log_knock($level) {
+	$HostInfo	 = 'Timestamp: '	. date('d-m-Y G:i:s')."\n";
+	$HostInfo	.= 'Request: '		. (isset($_SERVER['REQUEST_URI']) 				? $_SERVER['REQUEST_URI'] 				: 'Undefined')		."\n";
+	$HostInfo	.= 'Proxy?: '		. (isset($_SERVER['HTTP_X_FORWARDED_FOR']) 		? $_SERVER['HTTP_X_FORWARDED_FOR']		: 'Probably not')	."\n";
+	$HostInfo	.= 'IP Address: '	. (isset($_SERVER['REMOTE_ADDR']) 				? $_SERVER['REMOTE_ADDR'] 				: 'Undefined')		."\n";
+	$HostInfo	.= 'User-Agent:'	. (isset($_SERVER['HTTP_USER_AGENT']) 			? $_SERVER['HTTP_USER_AGENT'] 			: 'Undefined')		."\n\n";
+	
+	if (KKA_LOG_FILE_LEVEL == $level) {
+		log_to_file($HostInfo);
+	}
+	
+	if (KKA_LOG_EMAIL_LEVEL == $level) {
+		log_to_email($HostInfo);
+	}
+}
+
+/**
+ * Show a fake 404 page and die
+ */
+function fake_404() {
+    header('HTTP/1.1 404 Not Found');
+    exit();
+}
 
 // We get the knock (i.e. /path/?pass we get the "pass" string.)
 if (isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING'])) {
@@ -50,13 +97,13 @@ if ($knock == KKA_CLOSE_SESSION) {
     if (file_exists(KKA_FILE_NAME)) {
         unlink(KKA_FILE_NAME);
     }
-    header('HTTP/1.1 404 Not Found');
-    exit();
+    fake_404();
 }
 
 // Not the first try?
 if (file_exists(KKA_FILE_NAME)) {
-	$succeed_try = file_get_contents(KKA_FILE_NAME); // Counter to match with $possible_tries
+    // How many succefull knocks were made?
+	$succeed_try = file_get_contents(KKA_FILE_NAME); 
 } else {
 	$succeed_try = 0;
 }
@@ -75,35 +122,8 @@ if ($succeed_try != count($passphrase)) {
 		// Start again
 		file_put_contents(KKA_FILE_NAME, 0);
 	}
-	header('HTTP/1.1 404 Not Found');
-	exit();
+    fake_404();
 }
 
 // Cleanup
 unset($knock, $succeed_try, $passphrase);
-
-
-// Return timestamp, request, IP and user agent for loging
-function log_knock($level){
-	$HostInfo	 = 'Timestamp: '	. date('d-m-Y G:i:s')."\n";
-	$HostInfo	.= 'Request: '		. (isset($_SERVER['REQUEST_URI']) 				? $_SERVER['REQUEST_URI'] 				: 'Undefined')		."\n";
-	$HostInfo	.= 'Proxy?: '		. (isset($_SERVER['HTTP_X_FORWARDED_FOR']) 		? $_SERVER['HTTP_X_FORWARDED_FOR']		: 'Probably not')	."\n";
-	$HostInfo	.= 'IP Address: '	. (isset($_SERVER['REMOTE_ADDR']) 				? $_SERVER['REMOTE_ADDR'] 				: 'Undefined')		."\n";
-	$HostInfo	.= 'User-Agent:'	. (isset($_SERVER['HTTP_USER_AGENT']) 			? $_SERVER['HTTP_USER_AGENT'] 			: 'Undefined')		."\n\n";
-	
-	if (KKA_LOG_FILE_LEVEL == $level) {
-		log_to_file($HostInfo);
-	}
-	
-	if (KKA_LOG_EMAIL_LEVEL == $level) {
-		log_to_email($HostInfo);
-	}
-}
-
-function log_to_file($what){
-	file_put_contents(KKA_LOG_FILE_NAME, $what, FILE_APPEND);
-}
-
-function log_to_email($what){
-	mail(KKA_LOG_EMAIL_ADDRESS, 'Knock Knock Admin log', $what);
-}
